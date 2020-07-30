0e87b08d0f0b2509880ddb9bac3f92d653b3ce13
Gerard Darris <darrisgr@gmail.com>
2020-07-29 20:01:23 -0500

    Changed crew and header components as per studio 2 instructions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -10,8 +10,29 @@ export class CrewComponent implements OnInit {
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
+    {name: "Gerard Darris", firstMission:true}
   ];
+
+  memberBeingEdited: object = null;
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+  let index = this.crew.indexOf(member);
+  this.crew.splice(index, 1);
+}
+
+save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
 
   constructor() { }
 
